7a2638ae856e8b69b0e43dfb67b59ce27a2df5a1
James Hocking <jhoc6907@uni.sydney.edu.au>
2023-09-19 13:50:06 +1000

    fixing stuff

--- main.py
+++ main.py
@@ -19,16 +19,60 @@ T -> AA
 A -> a
 """
 
+TEST_1 = {
+    "V" :['S','X', 'Y', 'A', 'B'],
+    "T" : ['a', 'b'],
+    "start" : 'S',
+    "rules" : [('S', ('X', 'Y')),
+            ('S', ('A', 'X')),
+            ('S', ('B', 'Y')),
+            ('S', tuple()),
+            ('S', ('a',)),
+            ('S', ('b',)),
+            ('X', ('A', 'X')),
+            ('Y', ('B', 'Y')),
+            ('X', ('a',)),
+            ('Y', ('b',)),
+            ('A', ('a',)),
+            ('B', ('b',)),
+            ]
+}
+
 def get_new_cfg():
     # read a CFG via stdin. See parser.py for details on the returned object
     cfg = parse_cfg()
+    # cfg= TEST_1
+    rules_to_omit = []
+    for rule in cfg["rules"]:
+        left_side, right_side = rule
+        if (len(right_side) == 1 and left_side == "S"):
+            if right_side == ():
+                pass
+            else:
+                rules_to_omit.append(rule)
+
+    for rule in rules_to_omit:
+        cfg["rules"].remove(rule)
+
+
     # construct a new CFG, then output it to stdout.
     # find a rule where A -> a, X -> a, Y -> a
     final_variable = []
     for rule in cfg["rules"]:
         left_side, right_side = rule 
         if "a" in right_side:
-            final_variable.append(left_side)
+            # we need to check there are no other rules with that variable on the left
+            counter = 0
+            for new_rule in cfg["rules"]:
+                left, right = new_rule
+                if (left == left_side):
+                    counter += 1
+            if counter == 1:
+                final_variable.append(left_side)
+            else:
+                cfg["rules"].remove(rule)
+                cfg["rules"].append((left_side, tuple()))
+
     if final_variable == "":
         return cfg #if no rule then only accept 0 "a"'s -> SAME CFG as start
     
@@ -38,6 +82,7 @@ def get_new_cfg():
     two_a = []
     three_a = []
     four_a = []
+    single_rules = []
 
 
     new_Variables = []
@@ -52,6 +97,7 @@ def get_new_cfg():
             for i in range(5): #we make five copies
                 index = i
                 next_index = (index + 1) % 5
+               
                 if right_side[0] in final_variable:
                     new_rule = (left_side + str(index), (right_side[0], right_side[1] + str(next_index)))
                     new_Variables.append(left_side + str(index))
@@ -60,12 +106,14 @@ def get_new_cfg():
                     new_rule = (left_side + str(index), (right_side[0] + str(next_index), right_side[1]))
                     new_Variables.append(left_side + str(index))
                     new_Variables.append(right_side[0] + str(next_index))
+
                 else:
                     # theres no Variable which derives to a
-                    new_rule = (left_side + str(index), (right_side[0] + str(index) , right_side[1] + str(next_index)))
+                    new_rule = (left_side + str(index), (right_side[0] + str(index) , right_side[1] + str(index)))
                     new_Variables.append(left_side + str(index))
-                    new_Variables.append(right_side[1] + str(next_index))
+                    new_Variables.append(right_side[1] + str(index))
                     new_Variables.append(right_side[0] + str(index))
+
                 if i == 0:
                     no_a.append(new_rule)
                 if i == 1:
@@ -77,12 +125,19 @@ def get_new_cfg():
                 if i == 4:
                     four_a.append(new_rule)
         else:
-            new_rule = (left_side + str(0), right_side[0])  
+            if left_side in final_variable:
+                new_rule = (left_side, right_side)  
+            else:
+                new_rule = (left_side + str(0), right_side)
+                new_Variables.append(left_side + str(0))
+            single_rules.append(new_rule)
 
+    new_Variables = list(set(new_Variables)) #removes duplicates 
     
-    cfg["rules"] = no_a + one_a + two_a + three_a + four_a
+    cfg["rules"] = no_a + one_a + two_a + three_a + four_a + single_rules
     cfg["V"] = new_Variables
     cfg["start"] = "S0"
+
 
     return cfg
 
